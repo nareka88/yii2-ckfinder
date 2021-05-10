979348496db314ecac1d47bbdd9c5cbcf25a2adf
nareka88 <nareka88@gmail.com>
2021-05-10 18:57:29 +0430

    Initial commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace \nareka88\ckfinder;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# yii2-ckfinder
+ Integrated file-manager for CK-Editor
